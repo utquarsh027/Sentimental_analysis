5b3800b3cb20a8468d06eb08359a011bb3d57166
Utkarsh Jhariya <87616610+utquarsh027@users.noreply.github.com>
2024-05-03 15:14:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # core packages
 import streamlit as st
-# from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu
 import pandas as pd
 import numpy as np
 import tensorflow as tf
